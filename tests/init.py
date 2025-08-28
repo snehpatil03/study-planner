8bf3ac7135c6db8964cef5a5ec11e8083beb0798
snehpatil03 <patil.sneh1@northeastern.edu>
2025-08-28 18:01:51 -0400

    Adding init.py File in tests Folder

---
+++ tests/init.py
@@ -0,0 +1,8 @@
+"""
+tests package
+
+This package contains basic unit tests for the personalised learning assistant.
+The tests use fake embeddings and a fake language model to avoid calling
+external APIs.  To run the test suite install the requirements and execute
+`pytest` from the repository root.
+"""
